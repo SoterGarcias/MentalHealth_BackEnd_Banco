08cfe031aad3cb30ef14a19bccd7fd4de1f2a993
SoterGarcias <soter_garcia@hotmail.com>
2023-05-12 19:27:16 -0300

    ajuste de erros

--- src/modules/usuarios/usuarios.service.ts
+++ src/modules/usuarios/usuarios.service.ts
@@ -4,71 +4,73 @@ import { UsuariosDTO } from './usuarios.dto';
 
 @Injectable()
 export class UsuariosService {
+  constructor(private prisma: PrismaService) {}
 
-    constructor(private prisma: PrismaService) { }
+  // METODO PARA CRIAR UM USUARIO
+  async create(data: UsuariosDTO) {
+    const usuariosExists = await this.prisma.usuarios.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
 
-    //METODO PARA CRIAR UM USUARIO
-    async create(data: UsuariosDTO) {
-        const usuariosExists = await this.prisma.usuarios.findFirst({
-            where: {
-                email: data.email
-            }
-        })
-        if (usuariosExists) {
-            throw new Error("Email ja cadastrado!")
-        }
-
-        const usuarios = await this.prisma.usuarios.create({
-            data,
-        });
-
-        return usuarios;
-    }
-    findAll() {
-        return this.prisma.usuarios.findMany({
-            orderBy: [{
-                name: 'asc',
-            }]
-
-        })
+    if (usuariosExists) {
+      throw new Error('Email já cadastrado!');
     }
 
-    //METODO PARA ALTERAR UM USUARIO COM PASSAGEM DE PARAMETRO
-    async update(id: number, data: UsuariosDTO) {
-        const usuariosExists = await this.prisma.usuarios.findFirst({
-            where: {
-                id,
-            },
-        });
+    const usuarios = await this.prisma.usuarios.create({
+      data,
+    });
 
-        if (!usuariosExists) {
-            throw new Error("Usuario nao cadastrado!")
-        }
+    return usuarios;
+  }
 
-        return await this.prisma.usuarios.update({
-            data,
-            where: {
-                id,
-            },
-        });
-    }
-    //METODO PARA DELETAR UM USUARIO
-    async delete(id: number) {
-        const usuariosExists = await this.prisma.usuarios.findFirst({
-            where: {
-                id,
-            },
-        });
+  findAll() {
+    return this.prisma.usuarios.findMany({
+      orderBy: [
+        {
+          name: 'asc',
+        },
+      ],
+    });
+  }
 
-        if (!usuariosExists) {
-            throw new Error("Usuario nao cadastrado!")
-        }
+  // METODO PARA ALTERAR UM USUARIO COM PASSAGEM DE PARAMETRO
+  async update(id: number, data: UsuariosDTO) {
+    const usuariosExists = await this.prisma.usuarios.findFirst({
+      where: {
+        id,
+      },
+    });
 
-        return await this.prisma.usuarios.delete({            
-            where: {
-                id,
-            },
-        });        
+    if (!usuariosExists) {
+      throw new Error('Usuário não cadastrado!');
     }
 
+    return await this.prisma.usuarios.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  //METODO PARA DELETAR UM USUARIO
+  async delete(id: number) {
+    const usuariosExists = await this.prisma.usuarios.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!usuariosExists) {
+      throw new Error('Usuário não cadastrado!');
+    }
+
+    return await this.prisma.usuarios.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
